9011532970c0c121a95750fa8366d1ac63535786
Michael <captkit@ukr.net>
2025-05-11 14:25:43 +0300

    correction 1

--- js/task-3.js
+++ js/task-3.js
@@ -10,8 +10,6 @@ function getElementWidth(content, padding, border) {
     return totalWidth;
   }
   
-getElementWidth(50, 8, 4);
-
   console.log(getElementWidth("50px", "8px", "4px")); 
   console.log(getElementWidth("60px", "12px", "8.5px")); 
   console.log(getElementWidth("200px", "0px", "0px")); 
